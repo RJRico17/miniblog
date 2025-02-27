7c283b5bdd906a81510309d494fdde9f5e2b89f7
Rj-fredrick Rico <rico.rj-fredrick@student.greenriver.edu>
2025-02-27 14:33:47 -0800

    added sql

---
+++ miniblog.sql
@@ -0,0 +1,11 @@
+-- CREATE DATABASE miniblog;
+USE miniblog;
+
+-- CREATE TABLE posts (
+-- 	ID INT auto_increment,
+--     author VARCHAR(255),
+--     title VARCHAR(255),
+--     content MEDIUMTEXT,
+--     
+--     PRIMARY KEY (ID)
+-- );
